ce897613d0dc9cf70f72655f243ccb8c763af0bb
Fred Leal <flm_@msn.com>
2021-09-22 13:18:28 -0300

    Component Header Criada e parcialmente estilizada

---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import './header.css'
+
+export default function Header(){
+    return (
+        <header className="header">
+            <section className="header-container">
+                <div className="logo">
+                    <img src="logo-corebiz.svg" alt="Logo"/>
+                </div>
+                <nav className="navbar">
+                    <ul className="navbar-itens">
+                        <li className="nav-item">
+                            <a id="nav-item-corebiz" target="_blank" rel="noreferrer noopener"
+                            href="https://www.corebiz.ag/pt/about/">a corebiz</a>
+                        </li>
+                        <li className="nav-item">
+                            <a id="nav-item-services" target="_blank" rel="noreferrer noopener"
+                            href="https://www.corebiz.ag/pt/#framework-title">serviços</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a id="nav-item-cases" target="_blank" rel="noreferrer noopener"
+                            href="https://www.corebiz.ag/pt/cases/">cases</a>
+                        </li>
+                        <li className="nav-item">
+                            <a id="nav-item-contact" target="_blank" rel="noreferrer noopener"
+                            href="https://www.corebiz.ag/pt/contato/">contato</a>
+                        </li>
+                    </ul> 
+                </nav>
+                </section>
+        </header>
+    );
+}
